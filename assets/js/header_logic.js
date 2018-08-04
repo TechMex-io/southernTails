dc328abf93ec299fa857267615a16b54b904e7b5
George Ramirez <george.ramirez2@gmail.com>
2018-08-04 14:18:49 -0600

    commented out one of the header js file, as it was giving errors

--- assets/js/header_logic.js
+++ assets/js/header_logic.js
@@ -1,28 +1,27 @@
 
-document.addEventListener("scroll", function() {
+// document.addEventListener("scroll", function() {
 
-  const nav = document.getElementById('navBar')
-  const logo = document.getElementById('navLogo')
+//   const nav = document.getElementById('navBar')
+//   const logo = document.getElementById('navLogo')
 
-    if (window.pageYOffset > 300){
-      // nav.classList.remove('uk-hidden')
-      nav.classList.add('bg-purple')
-      logo.classList.remove('uk-invisible')
-    }
-    if (window.pageYOffset < 300){
-      nav.classList.remove('bg-purple')
-      nav.classList.add('bg-clear')
-      logo.classList.add('uk-invisible')
-    }
-  })
+//     if (window.pageYOffset > 300){
+//       nav.classList.add('bg-purple')
+//       logo.classList.remove('uk-invisible')
+//     }
+//     if (window.pageYOffset < 300){
+//       nav.classList.remove('bg-purple')
+//       nav.classList.add('bg-clear')
+//       logo.classList.add('uk-invisible')
+//     }
+//   })
 
   
-  const shiftWindow = function() { scrollBy(0, -60) };
-  if (location.hash) shiftWindow() 
+//   const shiftWindow = function() { scrollBy(0, -60) };
+//   if (location.hash) shiftWindow() 
 
-  window.addEventListener("hashchange", shiftWindow);
+//   window.addEventListener("hashchange", shiftWindow);
  
 
-  setTimeout(function(){
-    UIkit.modal("#modal-center").show();
-  }, 3000)
+//   setTimeout(function(){
+//     UIkit.modal("#modal-center").show();
+//   }, 3000)
